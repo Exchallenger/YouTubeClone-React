6bb6873c9014c96d3263a44af15cd98eb537eb87
exchallenger <qkwl1013@naver.com>
2020-12-20 18:18:42 +0900

    Make navbar CSS

--- .history/src/components/navbar/navbar.module_20201220170435.jsx
+++ .history/src/components/navbar/navbar.module_20201220170435.jsx
@@ -7,10 +7,11 @@ const Navbar = () => {
         <span className={styles.logo}>
             <span className={styles.logo_img}><i className="fab fa-youtube"></i></span>
             <span className={styles.logo_name}>ExchTube</span>
-        </span>
-        <span className={styles.input_form}>
-            <input className={styles.input} type="text" placeholder="Search"></input>
-            <button className={styles.btn}><i className="fas fa-search"></i></button>
+            <span className={styles.input_form}>
+                    <input className={styles.input} type="text" placeholder="Search"></input>
+                    <button className={styles.btn}><i className="fas fa-search"></i></button>
+            </span>
+
         </span>    
     </form>);
 };
